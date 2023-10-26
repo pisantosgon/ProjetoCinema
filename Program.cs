765e124a2fe17021d1fab92b0b4fbb6b5ff80d65
pietra <15pietrasantos@email.com>
2023-10-26 10:00:19 -0300

    Alteração stribg de conexão.

--- Program.cs
+++ Program.cs
@@ -6,8 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<Contexto>
-    (options => options.UseSqlServer("Data Source=SB-1490642\\SQLSENAI;Initial Catalog = ProjetoCinema;Integrated Security = True;TrustServerCertificate = True"));
+/*builder.Services.AddDbContext<Contexto> //Pietra
+    (options => options.UseSqlServer("Data Source=SB-1490642\\SQLSENAI;Initial Catalog = ProjetoCinema;Integrated Security = True;TrustServerCertificate = True"));*/
+
+/*builder.Services.AddDbContext<Contexto> //Heloísa
+    (options => options.UseSqlServer("Data Source=SB-1490640\\SQLSENAI;Initial Catalog = ProjetoCinema;Integrated Security = True;TrustServerCertificate = True"));*/
+
+
+/*builder.Services.AddDbContext<Contexto> // Maria Heloísa
+    (options => options.UseSqlServer("Data Source=SB-1490647\\SQLSENAI;Initial Catalog = ProjetoCinema;Integrated Security = True;TrustServerCertificate = True"));*/
+
+
+/*builder.Services.AddDbContext<Contexto> //  Maria Eduarda
+    (options => options.UseSqlServer("Data Source=SB-1490648\\SQLSENAI;Initial Catalog = ProjetoCinema;Integrated Security = True;TrustServerCertificate = True"));*/
+
 
 var app = builder.Build();
 
