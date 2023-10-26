b403d8d031b7329384b62589967d1d4acc5a18b9
pietra <15pietrasantos@email.com>
2023-10-26 08:27:53 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoCinema.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+
+    }
+}
